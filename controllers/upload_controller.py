6e50e04433a3516b6f1b1416979ad112e36dad31
shravani2022 <shravanishedge22@gmail.com>
2026-07-15 22:59:04 +0530

    Add Excel service and update upload functionality

--- controllers/upload_controller.py
+++ controllers/upload_controller.py
@@ -1,19 +1,43 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
-from database.db import mysqlconnection
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
 
-upload_bp = Blueprint('upload',__name__)
+upload_bp = Blueprint('upload', __name__)
 
+UPLOAD_FOLDER = "uploads"
 
 @upload_bp.route('/upload_invoice')
 def upload_invoice():
     return render_template('upload_invoice.html')
 
 
-@upload_bp.route('/upload_invoice',method=['POST'])
+@upload_bp.route('/upload_invoice', methods=['POST'])
 def upload_invoice_post():
-    
+
     file = request.files['invoice_file']
+
+    if file.filename == "":
+        return "Please Select File"
+
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    filename = secure_filename(file.filename)
+
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+    file.save(filepath)
     
-    print(file.filename)
+    df = pd.read_excel(filepath)
     
-    return "File Received Successfully"
+    print("First 5 rows")
+    df.head(5)
+    
+    print("Last 5 rows")
+    df.tail(5)
+    
+
+    print("Saved at:", os.path.abspath(filepath))
+
+    return redirect(url_for('upload.upload_invoice'))
